6a58a6b3c41ec6483dc8407e6d9020eb0ac1999f
Daniel Souza <danielfsouza@gmail.com>
2021-05-14 11:02:02 -0300

    Curso Java

---
+++ src/course/aula1.java
@@ -0,0 +1,14 @@
+package course;
+
+public class aula1 {
+
+	public static void main(String[] args) {
+		Pessoa p = new Pessoa("João",20,"M");
+		Pessoa p1 = new Pessoa("Maria",23,"F");
+		
+		System.out.println(p.toString());
+		System.out.println(p1.toString());
+
+	}
+
+}
